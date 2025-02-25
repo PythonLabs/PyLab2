cdad7b658f858d9b1b839507b961a4ae22f318ef
Евгений Каличак <88834448+Nekori04@users.noreply.github.com>
2025-02-25 09:55:59 +0300

    Update well-done

--- well-done/algoritmic.py
+++ well-done/algoritmic.py
@@ -1,20 +1,26 @@
 import itertools
 import re
 
+#Добавить возможность ввода своих значений от пользователя
+
 class SequenceCounter:
-    @staticmethod
-    def count_sequences():
-        alphabet = ['К', 'А', 'Т', 'Е', 'Р']
+    def count_sequences(self, word):
+        alphabet = list(word)
         count = 0
-        for seq in itertools.product(alphabet, repeat=6):
-            if seq[0] == 'Р' and seq[-1] == 'К':
+        for seq in itertools.product(alphabet, repeat=len(word)):
+            if seq[0] == word[-1] and seq[-1] == word[0]:
                 count += 1
         return count
 
+word = input("Введите слово: ")
+counter = SequenceCounter()
+print("Количество последовательностей:", counter.count_sequences(word))
+
+
 class ExpressionCalculator:
     @staticmethod
-    def calculate_expression():
-        value = 216 ** 6 + 216 ** 4 + 36 ** 6 - 6 ** 14 - 24
+    def calculate_expression(base):
+        value = base ** 6 + base ** 4 + (base // 6) ** 6 - 6 ** 14 - 24
         return ExpressionCalculator.to_base_6(value)
 
     @staticmethod
@@ -25,12 +31,15 @@ class ExpressionCalculator:
             n //= 6
         return len(set(digits[::-1]))
 
+base = int(input("Введите основание степени: "))
+print("Результат выражения:", ExpressionCalculator.calculate_expression(base))
+
 class NumberFinder:
     @staticmethod
-    def find_numbers():
+    def find_numbers(start, end):
         pattern = re.compile(r'12345\d7\d8')
         numbers = []
-        for number in range(123450708, 123459798 + 1):
+        for number in range(start, end + 1):
             if number > 10**9:
                 break
             if pattern.match(str(number)) and number % 23 == 0:
@@ -38,12 +47,11 @@ class NumberFinder:
         return numbers
 
     @staticmethod
-    def create_table():
-        numbers = NumberFinder.find_numbers()
+    def create_table(start, end):
+        numbers = NumberFinder.find_numbers(start, end)
         return [(number, number // 23) for number in sorted(numbers)]
 
-# Вывод результатов
-print(SequenceCounter.count_sequences())
-print(ExpressionCalculator.calculate_expression())
-for row in NumberFinder.create_table():
+start = int(input("Введите начальное значение диапазона: "))
+end = int(input("Введите конечное значение диапазона: "))
+for row in NumberFinder.create_table(start, end):
     print(row)
